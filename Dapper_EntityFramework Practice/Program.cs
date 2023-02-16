3c2faee267317f66cebc4670ff33b8240c112445
jose sanchez <jose.sanchez.artist@gmail.com>
2023-02-15 16:53:49 -0800

    added select statement

--- Dapper_EntityFramework Practice/Program.cs
+++ Dapper_EntityFramework Practice/Program.cs
@@ -9,15 +9,15 @@ IDbConnection dbConnection = new SqlConnection(connectionString);
 
 string sqlCommand = "SELECT GETDATE()";
 
-DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand); 
+DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
 
-Console.WriteLine($"Current Date and time: {rightNow}");
+//Console.WriteLine($"Current Date and time: {rightNow}");
 
 var myComputer = new Computer();
 {
     myComputer.Motherboard = "Z690";
     myComputer.HasWifi = true;
-    myComputer.HasLTE = true;
+    myComputer.HasLTE = false;
     myComputer.ReleaseDate = DateTime.Now;
     myComputer.Price = 943.87m;
     myComputer.VideoCard = "RTX 2060";
@@ -32,7 +32,7 @@ string sql = @"INSERT INTO TutorialAppSchema.Computer(
     ReleaseDate,
     Price,
     VideoCard
-)VALUES ('"+myComputer.Motherboard 
+)VALUES ('" + myComputer.Motherboard
 + "','" + myComputer.HasWifi
 + "','" + myComputer.HasLTE
 + "','" + myComputer.ReleaseDate
@@ -41,9 +41,33 @@ string sql = @"INSERT INTO TutorialAppSchema.Computer(
 
 + "')";
 
-Console.WriteLine(sql); 
+//Console.WriteLine(sql);
 
-var result= dbConnection.Execute(sql);
+//var result = dbConnection.Execute(sql);
 
-Console.WriteLine(result.ToString());
+//Console.WriteLine(result.ToString());
+
+string sqlSelect = @"SELECT    
+    Computer.Motherboard,
+    Computer.HasWifi,
+    Computer.HasLTE,
+    Computer.ReleaseDate,
+    Computer.Price,
+    Computer.VideoCard
+    FROM TutorialAppSchema.Computer";
+
+IEnumerable<Computer> computers = dbConnection.Query<Computer>(sqlSelect);
+Console.WriteLine("'Motherboard', 'HasWifi', 'HasLTE', 'ReleaseDate'," +
+    "'Price', 'VideoCard'");
+
+foreach (Computer singleComputer in computers)
+{
+    Console.WriteLine("'" + myComputer.Motherboard
+        + "','" + myComputer.HasWifi
+        + "','" + myComputer.HasLTE
+        + "','" + myComputer.ReleaseDate
+        + "','" + myComputer.Price
+        + "','" + myComputer.VideoCard
+        + "'");
+}
 
