fd5297989a65a5e33192d73407409ef6ff4901ad
jose sanchez <jose.sanchez.artist@gmail.com>
2023-02-15 16:29:21 -0800

    Execute cmd added

---
+++ Dapper_EntityFramework Practice/Computer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper_EntityFramework_Practice
+{
+    public class Computer
+    {
+        public string Motherboard { get; set; }
+        public bool HasWifi { get; set; }
+        public bool HasLTE { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+        public string VideoCard { get; set; }   
+
+    }
+}
--- Dapper_EntityFramework Practice/Program.cs
+++ Dapper_EntityFramework Practice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper;
+using Dapper_EntityFramework_Practice;
 using Microsoft.Data.SqlClient;
 using System.Data;
 
@@ -11,3 +12,38 @@ string sqlCommand = "SELECT GETDATE()";
 DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand); 
 
 Console.WriteLine($"Current Date and time: {rightNow}");
+
+var myComputer = new Computer();
+{
+    myComputer.Motherboard = "Z690";
+    myComputer.HasWifi = true;
+    myComputer.HasLTE = true;
+    myComputer.ReleaseDate = DateTime.Now;
+    myComputer.Price = 943.87m;
+    myComputer.VideoCard = "RTX 2060";
+
+};
+
+
+string sql = @"INSERT INTO TutorialAppSchema.Computer(
+    Motherboard,
+    HasWifi,
+    HasLTE,
+    ReleaseDate,
+    Price,
+    VideoCard
+)VALUES ('"+myComputer.Motherboard 
++ "','" + myComputer.HasWifi
++ "','" + myComputer.HasLTE
++ "','" + myComputer.ReleaseDate
++ "','" + myComputer.Price
++ "','" + myComputer.VideoCard
+
++ "')";
+
+Console.WriteLine(sql); 
+
+var result= dbConnection.Execute(sql);
+
+Console.WriteLine(result.ToString());
+
